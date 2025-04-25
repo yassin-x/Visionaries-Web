349b2964e2d89b21cd2f2e58d7d2842c9ea555cb
Yassin Ibrahim <yassin.ibrahim.hamed@gmail.com>
2025-04-25 20:55:22 +0300

    header edit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Link from "../Link";
 import { Routes } from "@/constants/enums";
 import Nav from "./Nav";
-import Image from "next/image";
 
 export default function Header() {
   return (
@@ -14,14 +13,6 @@ export default function Header() {
               className="text-2xl font-bold text-primary hover:text-primary/90
               transition-colors duration-200 ease-in-out flex items-center gap-4"
             >
-              <Image
-                src={"/images/logo.png"}
-                width={980}
-                height={1080}
-                alt="Visionaries logo"
-                priority
-                className="w-7 h-7"
-              />
               Visionaries
             </h1>
           </Link>
