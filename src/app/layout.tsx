a807039e2309bc0161154c637765d9812f7cd7d4
Yassin Ibrahim <119485168+yassin-x@users.noreply.github.com>
2026-05-03 16:18:03 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Readex_Pro } from "next/font/google";
 import Header from "@/components/Header";
@@ -31,11 +32,19 @@ export default function RootLayout({
   return (
     <html lang={Languages.EN} data-theme="dark">
       <body className={`${readex.className}`}>
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N8G6HV5Q');</script>
+<Script
+  id="gtm"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-N8G6HV5Q');
+    `,
+  }}
+/>
         <Background />
         <Header />
         {children}
