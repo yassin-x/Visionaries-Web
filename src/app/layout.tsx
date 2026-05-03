b328f7ca34f9ef274c4204e56d1419b393bad925
Yassin Ibrahim <119485168+yassin-x@users.noreply.github.com>
2026-05-03 16:15:42 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,14 +30,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang={Languages.EN} data-theme="dark">
-      <head>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <body className={`${readex.className}`}>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-N8G6HV5Q');</script>
-      </head>
-      <body className={`${readex.className}`}>
         <Background />
         <Header />
         {children}
